757eb979a154db3d52c8cec8523eb144ef5feec0
Ro Wright <rwright@berkeley.edu>
2022-12-01 10:19:45 -0800

    Implement day 1 part 2

--- day1.py
+++ day1.py
@@ -1,14 +1,23 @@
 from lib.handle_input import read_input
-import pdb
 
 def part1():
-    elves = read_input('data/day01.txt')
-    calories = [[int(item) for item in elf] for elf in elves]
-    total_calories = [sum(items) for items in calories]
+    total_calories = generate_calorie_list()
     return max(total_calories)
 
+def part2():
+    total_calories = generate_calorie_list()
+    total_calories.sort(reverse=True)
+    top_three = total_calories[:3]
+    return sum(top_three)
+
+def generate_calorie_list():
+    elves = read_input('data/day01.txt')
+    calories = [[int(item) for item in elf] for elf in elves]
+    return [sum(items) for items in calories]
+
 def main():
-    print(part1())
+    print("The most calories an Elf is carrying is {}".format(part1()))
+    print("The calories carried by the three elves totals {}".format(part2()))
 
 if __name__ == '__main__':
     main()
