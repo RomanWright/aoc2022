a7a7226c828268ca3a3da8ea4c5c5fae463c79a1
Ro Wright <rwright@berkeley.edu>
2022-12-03 13:41:38 -0800

    Implement day 3 part 2

--- day03.py
+++ day03.py
@@ -17,9 +17,22 @@ def part1():
         total_priority += prioritize(shared.pop())
     return total_priority
 
+def part2():
+    rucksacks = format_data(read_input('data/day03.txt'))
+    i = 0
+    total_priority = 0
+    while i < len(rucksacks):
+        member1 = set(rucksacks[i])
+        member2 = set(rucksacks[i + 1])
+        member3 = set(rucksacks[i + 2])
+        shared = member1.intersection(member2, member3)
+        total_priority += prioritize(shared.pop())
+        i += 3
+    return total_priority
 
 def main():
-    print('The total priority is {}'.format(part1()))
+    print('The total priority of each rucksack is {}'.format(part1()))
+    print('The total priority of each group is {}'.format(part2()))
 
 if __name__ == '__main__':
     main()
