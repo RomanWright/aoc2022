178def0acb96380de51a28f25eaaaad1f59f1c23
Ro Wright <rwright@berkeley.edu>
2022-12-06 08:41:41 -0800

    Implement day 6 part 2

--- day06.py
+++ day06.py
@@ -2,16 +2,24 @@ from lib.handle_input import read_input
 
 def part1():
     data = read_input('data/day06.txt')
+    return find_marker(data, 4)
+
+def find_marker(data, marker_length):
     index = 0
-    while index < len(data) - 3:
-        test = set(data[index:index+4])
-        if len(test) == 4:
-            return index + 4
+    while index <= len(data) - marker_length:
+        test = set(data[index:index+marker_length])
+        if len(test) == marker_length:
+            return index + marker_length
         index += 1
     return None
 
+def part2():
+    data = read_input('data/day06.txt')
+    return find_marker(data, 14)
+
 def main():
-    print('The first marker ends at {}'.format(part1()))
+    print('The packet marker ends at {}'.format(part1()))
+    print('The message marker ends at {}'.format(part2()))
 
 if __name__ == '__main__':
     main()
