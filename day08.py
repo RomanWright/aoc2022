782599d1f03d635ccb2d2296bee079c854153941
Ro Wright <rwright@berkeley.edu>
2022-12-08 11:44:33 -0800

    Implement day 8 part 2

--- day08.py
+++ day08.py
@@ -11,13 +11,9 @@ def part1(data):
 def is_visible(data, x, y):
     visible = False
     height = data[x][y]
-    # Left
     visible = visible or is_tallest(height, data[x][:y])
-    # Right
     visible = visible or is_tallest(height, data[x][y+1:])
-    # Top
     visible = visible or is_tallest(height, [datum[y] for datum in data[:x]])
-    # Bottom
     visible = visible or is_tallest(height, [datum[y] for datum in data[x+1:]])
     return visible
 
@@ -28,9 +24,54 @@ def is_tallest(height, others):
         return True
     return False
 
+def part2(data):
+    best_score = 0
+    for x in range(len(data)):
+        for y in range(len(data[x])):
+            if scenic_score(data, x, y) > best_score:
+                best_score = scenic_score(data, x, y)
+    return best_score
+
+def scenic_score(data, x, y):
+    height = data[x][y]
+
+    score1 = 0
+    testx, testy = x, y - 1
+    while testy >= 0 and data[testx][testy] <= height:
+        score1 += 1
+        if data[testx][testy] == height:
+            break
+        testy -= 1
+
+    score2 = 0
+    testx, testy = x, y + 1
+    while testy < len(data[testx]) and data[testx][testy] <= height:
+        score2 += 1
+        if data[testx][testy] == height:
+            break
+        testy += 1
+    
+    score3 = 0
+    testx, testy = x - 1, y
+    while testx >= 0 and data[testx][testy] <= height :
+        score3 += 1
+        if data[testx][testy] == height:
+            break
+        testx -= 1
+
+    score4 = 0
+    testx, testy = x + 1, y
+    while testx < len(data) and data[testx][testy] <= height:
+        score4 += 1
+        if data[testx][testy] == height:
+            break
+        testx += 1
+    return score1 * score2 * score3 * score4
+
 def main():
     data = read_input('data/day08.txt').split('\n')
     print('There are {} visible trees'.format(part1(data)))
+    print('The highest scenic score is {}'.format(part2(data)))
 
 if __name__ == '__main__':
     main()
