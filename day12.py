883f8e6f2a27cac261ff05461331a9648e98ba25
Ro Wright <rwright@berkeley.edu>
2022-12-12 11:36:17 -0800

    Implement day 12 part 2

--- day12.py
+++ day12.py
@@ -57,19 +57,44 @@ def part1(data):
         positionmap.append(rowmap)
     end.cost = 0
     fringe = [end]
-    counter = 0
     while len(fringe) > 0:
         current = fringe.pop(0)
-        counter += 1
         for neighbor in current.find_walkable_neighbors():
             if (not neighbor.cost) or (neighbor.cost > current.cost + 1):
                 neighbor.cost = current.cost + 1
                 fringe.append(neighbor)
     return start.cost
 
+def part2(data):
+    positionmap = []
+    end = None
+    for i in range(len(data)):
+        rowmap = []
+        for j in range(len(data[i])):
+            position = Position(j, i, data, positionmap)
+            rowmap.append(position)
+            if data[i][j] == 'S':
+                start = position
+            if data[i][j] == 'E':
+                end = position
+        positionmap.append(rowmap)
+    end.cost = 0
+    fringe = [end]
+    while len(fringe) > 0:
+        current = fringe.pop(0)
+        for neighbor in current.find_walkable_neighbors():
+            if (not neighbor.cost) or (neighbor.cost > current.cost + 1):
+                neighbor.cost = current.cost + 1
+                fringe.append(neighbor)
+    flat_map = [position for row in positionmap for position in row]
+    a_heights = [position for position in flat_map if position.height == 0 and position.cost]
+    a_heights.sort(key=lambda position: position.cost)
+    return a_heights[0].cost
+
 def main():
     data = read_input('data/day12.txt').split('\n')
     print('Steps: {}'.format(part1(data)))
+    print('Fewest steps: {}'.format(part2(data)))
 
 if __name__ == '__main__':
     main()
